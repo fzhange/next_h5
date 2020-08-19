03b14716b735928565c83b5dbbe93cda3ba953c6
fzhange <2861669535@qq.com>
2020-08-19 10:57:31 +0800

    feat: ant-mobile import

--- next.config.js
+++ next.config.js
@@ -3,15 +3,33 @@ const withCSS = require('@zeit/next-css');
 const withLess = require('@zeit/next-less');
 const withPlugins = require('next-compose-plugins');
 const logger = require("./tool_server/logger")(__filename);
-const BuildHash = '202008131400';
+const path = require('path');
 
 const stylePlugins = [
-  [
+  [ 
     withCSS,
+    {
+      cssModules: false,
+      cssLoaderOptions: {
+        importLoaders: 1,
+        minimize:true,
+        // localIdentName: "[local]___[hash:base64:5]",
+      }
+    }
   ],
   [
-    withLess,
+    /** 
+     * The stylesheet is compiled to .next/static/css
+     * 如果开启cssModule 会导致ant-design-mobile 样式匹配结果错乱；所以需要关闭
+    */
+    withLess,  
     {
+      cssModules:false, //http://www.ruanyifeng.com/blog/2016/06/css_modules.html CSS Modules 用法教程
+      cssLoaderOptions:{
+        importLoaders: 1,
+        minimize:true,
+        // localIdentName: "[local]___[hash:base64:5]", //localIdentName  CSS-Module 定制哈希类名
+      },
       lessLoaderOptions: {
         javascriptEnabled: true,
       },
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import {invoke_post,Loading} from "../common/index";
 import "./styles/index.less"
+
 import { Button,Modal } from 'antd-mobile';
 import 'antd-mobile/lib/button/style/index.less'
-import 'antd-mobile/lib/modal/style/index.less'
-
 
 
 export default class Login extends React.Component{
     constructor(props){
         super(props);
     }
+    btnClick(){
+        Loading.show();
+    }
 
     render(){
         return (
             <div>
-                首页
+                <Button onClick={this.btnClick.bind(this)} type="primary">primary</Button>
             </div>
         )
     }
